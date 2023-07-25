bc6b91c5d629cbfb45fc8c12edf12e381c9feb50
Vishaal Agartha <vishaalagartha@gmail.com>
2023-07-25 14:49:36 -0700

    temp commit

--- run2.sh
+++ run2.sh
@@ -4,12 +4,14 @@ sleep 10
 
 curl -O http://localhost:8080/jnlpJars/jenkins-cli.jar
 
+sleep 5
+
 java -jar jenkins-cli.jar -s http://localhost:8080 create-job spring-petclinic < jenkins-container/spring-petclinic.xml
 
-python3 jenkins_container/setup_sonar.py
+python3 jenkins-container/setup_sonar.py
 
-docker cp jenkins_container/hudson.plugins.sonar.SonarGlobalConfiguration.xml jenkins:/var/jenkins_home
-docker cp jenkins_container/sonar_token.txt jenkins:/var/jenkins_home
+docker cp jenkins-container/hudson.plugins.sonar.SonarGlobalConfiguration.xml jenkins:/var/jenkins_home
+docker cp jenkins-container/sonar_token.txt jenkins:/var/jenkins_home
 
 docker stop jenkins
 docker start jenkins
