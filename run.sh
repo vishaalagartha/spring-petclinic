799101dfa28a56e9147aab5b6f53f06a999bcf50
Vishaal Agartha <vishaalagartha@gmail.com>
2023-07-25 14:54:47 -0700

    Rename run 2

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
-docker build -t jenkins-image jenkins-container/
-
-docker run -d -p 8080:8080 -p 50022:22 -p 50000:50000 --name jenkins jenkins-image
+docker compose up --detach
 
 sleep 10
 
 curl -O http://localhost:8080/jnlpJars/jenkins-cli.jar
 
+sleep 5
+
 java -jar jenkins-cli.jar -s http://localhost:8080 create-job spring-petclinic < jenkins-container/spring-petclinic.xml
+
+python3 jenkins-container/setup_sonar.py
+
+docker cp jenkins-container/hudson.plugins.sonar.SonarGlobalConfiguration.xml jenkins:/var/jenkins_home
+docker cp jenkins-container/sonar_token.txt jenkins:/var/jenkins_home
+
+docker stop jenkins
+docker start jenkins
